ac3fbb0b34883070e054e1f561a58965d9bedb71
brewmasterbean <me@bradybean.com>
2020-07-06 08:06:09 -0600

    Added browser-sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require ('gulp');
 var sass = require ('gulp-sass');
+var bsync =  require ('browser-sync');
+var reload = bsync.reload;
+
+var SOURCEPATHS = {
+  sassSource : 'src/scss/*.scss'
+}
+
+var APPPATH = {
+  css : 'app/css'
+}
 
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src('SOURCEPATHS.sassSource')
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('APPPATH.css'));
+});
+
+gulp.task('serve', gulp.series('sass'), function(){
+  bsync.init
 });
 
 gulp.task('default', gulp.series('sass'));
